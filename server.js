233e189bdae131b0d1450e0fbc5b75b904bc940e
Elias Moreira <elias_moreirajunior@mastercard.com>
2017-01-25 16:25:06 +0000

    fixing server

---
+++ server.js
@@ -0,0 +1,14 @@
+/**
+ * Created by eliasmj on 25/01/2017.
+ */
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./build'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 9000);
