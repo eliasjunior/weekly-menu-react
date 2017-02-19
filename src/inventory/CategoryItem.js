76fe435a568eacc61dd7c4d63bb0f2db224cb803
Elias Moreira <elias_moreirajunior@mastercard.com>
2017-02-19 23:36:22 +0000

    shopping list

--- src/inventory/CategoryItem.js
+++ src/inventory/CategoryItem.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import ListItem from 'material-ui/List/ListItem';
+import CatIcon from 'material-ui/svg-icons/image/view-comfy';
+import ProdIcon from 'material-ui/svg-icons/action/stars'
+import {indigo200, purple500} from 'material-ui/styles/colors'
 
 export const CategoryItem = (props) => {
-    return  <ListItem key={props.id}>{props.name}</ListItem>
+
+    const listItem = (ingredients) => {
+
+        console.log(">>>", ingredients)
+        return ingredients.map((ingredient) => {
+            return <ListItem key={ingredient._id} primaryText={ingredient.name}
+                             leftIcon={<ProdIcon color={purple500}/>} />
+        })
+
+    }
+
+    return  <ListItem
+                key={props.id}
+                initiallyOpen={true}
+                primaryText={props.name}
+                leftIcon={<CatIcon color={indigo200}></CatIcon>}
+                primaryTogglesNestedList={true} nestedItems={listItem(props.ingredients)}>
+
+            </ListItem>
 }
--- src/inventory/ShoppingListComponent.js
+++ src/inventory/ShoppingListComponent.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import {CategoryList} from './CategoryList';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {AppWeekBar} from '../common/AppWeekBar';
+import ApiService from '../service/ApiService';
+
 
 class ShoppingListComponent extends React.Component {
 
@@ -12,14 +16,27 @@ class ShoppingListComponent extends React.Component {
     }
 
     componentDidMount() {
-        this.setState({listCategories:  [{name: 'test'}, {name: 'test 2'}]});
+        ApiService.get('category/week/shopping')
+            .then((response)=> {
 
+                console.log("****", response.data)
+
+                this.setState({
+                    listCategories: response.data
+                });
+
+            }).catch(reason => {console.error(reason)});
     }
 
     render() {
-        return (
-            <CategoryList list={this.state.listCategories} ></CategoryList>
-        );
+        return(
+            <MuiThemeProvider>
+                <div>
+                    <AppWeekBar title="Shopping List"></AppWeekBar>
+                    <CategoryList list={this.state.listCategories} ></CategoryList>
+                </div>
+            </MuiThemeProvider>
+        )
     }
 }
 
