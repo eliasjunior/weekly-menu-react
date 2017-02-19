f4a2c39634ebd408ab97aab3d3d8ae0b72afcdcc
Elias Moreira <elias_moreirajunior@mastercard.com>
2017-02-19 23:48:04 +0000

    shopping list, bug prod

--- src/inventory/CategoryItem.js
+++ src/inventory/CategoryItem.js
@@ -9,7 +9,7 @@ export const CategoryItem = (props) => {
     const listItem = (ingredients) => {
 
         console.log(">>>", ingredients)
-        
+
         return ingredients.map((ingredient) => {
             return <ListItem key={ingredient._id} primaryText={ingredient.name}
                              leftCheckbox={<Checkbox />} />
