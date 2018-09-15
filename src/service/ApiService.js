81a39344c9ef5e81aae716b03edf879fd42e421d
Elias Moreira <elias_moreirajunior@mastercard.com>
2018-09-15 18:52:59 +0100

    point to server

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-//const baseUrl = 'https://week-menu-api.herokuapp.com/';
-const baseUrl = 'http://localhost:3002/';
+const baseUrl = 'https://week-menu-api.herokuapp.com/';
+//const baseUrl = 'http://localhost:3002/';
 
 function getErrorFromResponse(reason) {
     const response = reason['response'];
