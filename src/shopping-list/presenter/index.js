b7c2c42bdc1074be2d1722a0ad663bfeffaff7d6
Elias Junior <emcjunior@gmail.com>
2020-05-12 23:17:35 +0100

    Update index.js

--- src/shopping-list/presenter/index.js
+++ src/shopping-list/presenter/index.js
@@ -1,1 +1,147 @@
 
+import ShoppingListUseCase from "../use-cases";
+import { requiredParameter } from "common/Util";
+
+const { saveShoppingListAsync, updateShoppingListAsync } = ShoppingListUseCase;
+
+export const postShoppingList = saveShoppingListAsync;
+export const putShoppingList = updateShoppingListAsync;
+
+export function buildShopListPayload({
+  byId = requiredParameter("byId"),
+  selected = requiredParameter("byId"),
+}) {
+  const result = Object.entries(byId).reduce((prev, [_, prod]) => {
+    prev.push({
+      id: prod.id,
+      recipes: prod.recipes ? prod.recipes : [],
+      selected: selected.filter((prodId) => prodId === prod.id).length === 1,
+    });
+
+    return prev;
+  }, []);
+
+  return { products: result };
+}
+
+export function normalizeCatProd({ state, catId, prodId, checked }) {
+  const { products, categories } = state;
+  const category = categories.byId[catId];
+  const addSimpleProduct = () => {
+    const product = products.byId[prodId];
+    if (!product) {
+      products.byId[prodId] = {
+        id: prodId,
+        recipes: [],
+      };
+    }
+  };
+  if (!category) {
+    // no category, means no prod as weel, state 0
+    categories.byId[catId] = {
+      id: catId,
+      prods: [prodId],
+    };
+    addSimpleProduct();
+    products.selected = [...products.selected, prodId];
+  } else {
+    if (!checked) {
+      // there is prod in the category, recipe add previously, state 1
+      if (!category.prods.includes(prodId)) {
+        category.prods = [...category.prods, prodId];
+        addSimpleProduct();
+      }
+      // state 2
+      // selected only belong to simple product and products.byId doesn't
+      products.selected = [...products.selected, prodId];
+    } else {
+      const product = products.byId[prodId];
+      const isProdInRecipe =
+        product && product.recipes && product.recipes.length > 0;
+
+      // remove from selected
+      products.selected = products.selected.filter((pId) => pId !== prodId);
+
+      // state 3
+      if (!isProdInRecipe) {
+        category.prods = category.prods.filter((pId) => pId !== prodId);
+        delete products.byId[prodId];
+        // remove cat if there is no prods
+        // state 4
+        if (category.prods.length === 0) {
+          delete categories.byId[catId];
+        }
+      }
+    }
+  }
+  return {
+    categories: { ...categories },
+    products: { ...products },
+    recipes: state.recipes,
+  };
+}
+
+export function normalizeProdRecipe({ state, recId, catId, prodId, checked }) {
+  const { products, categories, recipes } = state;
+
+  const product = products.byId[prodId];
+
+  const addProduct = () => {
+    const category = categories.byId[catId];
+    category.prods = [...category.prods, prodId];
+    products.byId[prodId] = {
+      id: prodId,
+      recipes: [recId],
+    };
+  };
+
+  //State has recipe
+  if (checked) {
+    delete recipes.byId[recId];
+    product.recipes = product.recipes.filter((rid) => rid !== recId);
+    const isProdNotOnSelectedList = !products.selected.includes(prodId);
+
+    // delete product if its not in another rec or in the selected list
+    if (product.recipes.length === 0 && isProdNotOnSelectedList) {
+      const category = categories.byId[catId];
+      category.prods = category.prods.filter((pId) => pId !== prodId);
+
+      // remove cat if there is no prods
+      if (category.prods.length === 0) {
+        delete categories.byId[catId];
+      }
+      // if prod is not in another recipe
+      delete products.byId[prodId];
+    }
+  } else {
+    recipes.byId[recId] = {
+      id: recId,
+    };
+    //State has no category yet
+    const category = categories.byId[catId];
+    if (!category) {
+      categories.byId[catId] = {
+        id: catId,
+        prods: [],
+      };
+      addProduct();
+      //State has no product
+    } else if (!product) {
+      addProduct();
+    } else {
+      //State has product and is also selected
+      if (!product.recipes) {
+        product.recipes = [recId];
+      } else {
+        //State has product and is also into another recipe
+        product.recipes = [...product.recipes, recId];
+      }
+    }
+  }
+  return {
+    ...state,
+    categories: { ...categories },
+    products: { ...products },
+    recipes: { ...recipes },
+  };
+}
