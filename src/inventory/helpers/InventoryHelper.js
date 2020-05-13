5e872e09b210d8e9b7cb2d72930bdd7ac48aee37
Elias Junior <emcjunior@gmail.com>
2020-05-13 18:30:45 +0100

    Update InventoryHelper.js

--- src/inventory/helpers/InventoryHelper.js
+++ src/inventory/helpers/InventoryHelper.js
@@ -4,9 +4,12 @@ export function loadProductsToCategory(categories, products) {
   if (!products.byId) {
     return [];
   }
-  return categories.reduce((prev, item) => {
-    item.products = item.catProds.map((prodId) => products.byId[prodId]);
-    prev.push(item);
+
+  return categories.reduce((prev, category) => {
+    category.products = category.catProds.map((prodId) =>
+      validateProd(prodId, products)
+    );
+    prev.push(category);
     return prev;
   }, []);
 }
@@ -15,9 +18,11 @@ export function loadCategoryWithProducts(categories, products) {
   if (!products.byId) {
     return [];
   }
-  return categories.reduce((prev, item) => {
-    item.products = item.catProds.map((prodId) => products.byId[prodId]);
-    prev.push(item);
+  return categories.reduce((prev, category) => {
+    category.products = category.catProds.map((prodId) =>
+      validateProd(prodId, products)
+    );
+    prev.push(category);
     return prev;
   }, []);
 }
@@ -45,33 +50,10 @@ export function normalizeCategory(categories) {
   return categories.reduce(makeByIdTable, { byId: {}, allIds: [] });
 }
 
-//TODO delete if not using 22/04, no use right now
-export function wrapProdsIntoCategories(
-  products,
-  { byId = requiredParameter("byId categoryMap") }
-) {
-  const catset = products.reduce((prev, prod) => {
-    const category = byId[prod.catId];
-    if (!category) {
-      throw new Error("Something wrong, no category for prod", prod.name);
-    }
-
-    if (!category.products) {
-      category.products = [];
-      category.products.push(prod);
-    } else {
-      const prodOfC = category.products
-        .filter((prodOfCat) => prodOfCat.id === prod.id)
-        .pop();
-
-      if (!prodOfC) {
-        category.products.push(prod);
-      } else {
-        console.error("there is a prod", prodOfC.name);
-      }
-    }
-    prev.add(category);
-    return prev;
-  }, new Set());
-  console.log(">>>", catset);
+function validateProd(prodId, products) {
+  const product = products.byId[prodId];
+  if (!product) {
+    requiredParameter("ProdId in cat");
+  }
+  return product;
 }
