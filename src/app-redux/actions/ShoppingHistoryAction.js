dd19b7e9951a4a4049d715698710a3bce02ad2ef
Elias Junior <emcjunior@gmail.com>
2020-05-16 22:29:39 +0100

    Update ShoppingHistoryAction.js

--- src/app-redux/actions/ShoppingHistoryAction.js
+++ src/app-redux/actions/ShoppingHistoryAction.js
@@ -1,19 +1,23 @@
 import { loadingSomething } from "./LoadingAction";
-import { putShoppingList, postShoppingList } from "shopping-list/presenter";
+import {
+  putShoppingList,
+  postShoppingList,
+  getShoppingHistory,
+} from "shopping-list/presenter";
 import { afterResquest, afterRequestError } from "./common";
 import { requiredParameter } from "common/Util";
 export const UPDATE_SHOPPING_LIST = "UPDATE_SHOPPING_LIST";
 export const CREATE_SHOPPING_LIST = "CREATE_SHOPPING_LIST";
+export const FETCH_SHOPPING_HISTORY = "FETCH_SHOPPING_HISTORY";
 
-// // not sure will be here
-// export function fetchShoppingList(list) {
-//   return {
-//     type: FETCH_SHOPPING_HISTORY,
-//     payload: {
-//       list,
-//     },
-//   };
-// }
+export function fetchShoppingHistory(shoppingHistory = []) {
+  return {
+    type: FETCH_SHOPPING_HISTORY,
+    payload: {
+      shoppingHistory,
+    },
+  };
+}
 
 export function createShoppingList(shopping) {
   return {
@@ -26,6 +30,17 @@ export function updateShoppingList(shoppingListId) {
   return {
     type: UPDATE_SHOPPING_LIST,
     payload: { shoppingListId },
+  };
+}
+
+export function fetchShoppingHistoryAsync() {
+  return async (dispatch) => {
+    try {
+      const data = await getShoppingHistory();
+      dispatch(fetchShoppingHistory(data));
+    } catch (error) {
+      afterRequestError(dispatch, error);
+    }
   };
 }
 
@@ -49,7 +64,7 @@ export function createShoppingListAsync(shoppingList) {
     try {
       const data = await postShoppingList(shoppingList);
       shoppingList.id = data.id;
-      dispatch(createShoppingList(shoppingList));
+      dispatch(createShoppingList(data));
 
       afterResquest(dispatch);
     } catch (error) {
