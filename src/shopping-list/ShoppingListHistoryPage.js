8989f0f52769a37411f68b6395025cceac21e0d0
Elias Junior <emcjunior@gmail.com>
2020-05-16 22:35:14 +0100

    Update ShoppingListHistoryPage.js

--- src/shopping-list/ShoppingListHistoryPage.js
+++ src/shopping-list/ShoppingListHistoryPage.js
@@ -5,57 +5,48 @@ import {
   Button,
   ListItemSecondaryAction,
 } from "@material-ui/core";
-import ShoppingListService from "./ShoppingListService";
-import AppWeekBar from "header/AppWeekBar";
-import { LOCATION } from "common/AppConstant";
+import { LOCATION, parentComponent } from "common/AppConstant";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setPageLocation, setPageTitle } from "app-redux/actions/PageAction";
+import { editShoppingListAction } from "app-redux/actions/ShoppingListAction";
 
-class ShoppingListHistoryPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [],
-    };
-  }
-  componentDidMount() {
-    ShoppingListService.get()
-      .then((response) => {
-        this.setState({ list: response });
-      })
-      .catch((reason) =>
-        this.props.onHandleMessage({ message: reason.message })
-      );
-  }
-  render() {
-    return (
-      <div>
-        <AppWeekBar title="Shopping history"></AppWeekBar>
-        <List>
-          {this.state.list.map((item) => {
-            return (
-              <ListItem key={item._id}>
-                <Button color="primary" variant="outlined">
-                  <Link to={`${LOCATION.shopping.path}/${item._id}`}>
-                    {item.name}
-                  </Link>
-                </Button>
-                <ListItemSecondaryAction>
-                  <Button color="secondary" variant="outlined">
-                    <Link
-                      to={`${LOCATION.editShoppingList.path}/${item._id}`}
-                      onClick={() => this.props.editShoppingList(item)}
-                    >
-                      EDIT
-                    </Link>
-                  </Button>
-                </ListItemSecondaryAction>
-              </ListItem>
-            );
-          })}
-        </List>
-      </div>
-    );
-  }
+function ShoppingListHistoryPage() {
+  const dispatch = useDispatch();
+  const shoppingHistory = useSelector((state) => state.shoppingHistory);
+  const productMap = useSelector((state) => state.products);
+
+  dispatch(setPageLocation(parentComponent.SHOPPING_LIST_PAGE));
+  dispatch(setPageTitle("Shopping history"));
+  console.log(">>", shoppingHistory);
+
+  return (
+    <List>
+      {shoppingHistory.map((item) => {
+        return (
+          <ListItem key={item.id}>
+            <Button color="primary" variant="outlined">
+              <Link to={`${LOCATION.shopping.path}/${item.id}`}>
+                {item.name}
+              </Link>
+            </Button>
+            <ListItemSecondaryAction>
+              <Button color="secondary" variant="outlined">
+                <Link
+                  to={`${LOCATION.editShoppingList.path}/${item.id}`}
+                  onClick={() => {
+                    dispatch(editShoppingListAction(item, productMap));
+                  }}
+                >
+                  EDIT
+                </Link>
+              </Button>
+            </ListItemSecondaryAction>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
 }
 
 export default ShoppingListHistoryPage;
