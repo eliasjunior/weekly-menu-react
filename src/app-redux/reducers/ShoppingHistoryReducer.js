dc3facdaa78bc4ddc96d30ca4e1ab8b41f22a479
Elias Junior <emcjunior@gmail.com>
2020-05-16 22:31:06 +0100

    Create ShoppingHistoryReducer.js

---
+++ src/app-redux/reducers/ShoppingHistoryReducer.js
@@ -0,0 +1,17 @@
+import {
+  CREATE_SHOPPING_LIST,
+  FETCH_SHOPPING_HISTORY,
+} from "app-redux/actions/ShoppingHistoryAction";
+
+const initialState = [];
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case CREATE_SHOPPING_LIST:
+      return [...state, payload];
+    case FETCH_SHOPPING_HISTORY:
+      return [...payload.shoppingHistory];
+    default:
+      return state;
+  }
+};
