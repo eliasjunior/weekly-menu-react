9108cbaf03eeddf707499a482dbdc3b3f298c273
Elias Junior <emcjunior@gmail.com>
2020-05-19 23:19:44 +0100

    Update ListFilterAction.js

--- src/app-redux/actions/ListFilterAction.js
+++ src/app-redux/actions/ListFilterAction.js
@@ -1,17 +1,24 @@
 export const SET_FILTER_NAME = "SET_FILTER_NAME";
 export const SET_DISPLAY_LIST = "SET_DISPLAY_LIST";
+export const FLAG_PICKED_SHOP_PROD = "FLAG_PICKED_SHOP_PROD";
 
 export function listFilterAction(textFilter, listDB) {
   return {
     type: SET_FILTER_NAME,
-    textFilter,
-    listDB
+    payload: { listDB, textFilter },
   };
 }
 
 export function setDisplatList(listDB) {
   return {
     type: SET_DISPLAY_LIST,
-    listDB
+    payload: { listDB },
+  };
+}
+
+export function flagPickedShotProd({ id, picked }) {
+  return {
+    type: FLAG_PICKED_SHOP_PROD,
+    payload: { id, picked },
   };
 }
