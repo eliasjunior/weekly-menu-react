9d8d1c3b2f28865441a19e826650663991823718
Elias Junior <emcjunior@gmail.com>
2020-05-19 23:24:31 +0100

    Update QuantityBtnInfo.js

--- src/components/QuantityBtnInfo.js
+++ src/components/QuantityBtnInfo.js
@@ -1,11 +1,31 @@
 import React from "react";
 import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
+import { ListItemSecondaryAction } from "@material-ui/core";
+import PlaylistAddCheck from "@material-ui/icons/PlaylistAddCheck";
+import { greenColor } from "styles/CommonStyles2";
+import { useDispatch } from "react-redux";
+import { flagPickedShotProd } from "app-redux/actions/ListFilterAction";
 
-export default function QuantityBtnInfo({ quantityDisplay, recDisplay }) {
+export default function QuantityBtnInfo({
+  quantityDisplay,
+  recDisplay,
+  id,
+  picked,
+}) {
+  const dispatch = useDispatch();
+  const handleUpdateShopList = () => {
+    dispatch(flagPickedShotProd({ id, picked: picked ? false : true }));
+  };
+
   return (
-    <Tooltip title={recDisplay ? recDisplay : "-"}>
-      <IconButton aria-label="delete">{quantityDisplay}</IconButton>
-    </Tooltip>
+    <ListItemSecondaryAction>
+      <IconButton aria-label="Comments" onClick={() => handleUpdateShopList()}>
+        <PlaylistAddCheck style={picked ? {} : greenColor} />
+      </IconButton>
+      <Tooltip title={recDisplay ? recDisplay : "-"}>
+        <IconButton aria-label="delete">{quantityDisplay}</IconButton>
+      </Tooltip>
+    </ListItemSecondaryAction>
   );
 }
