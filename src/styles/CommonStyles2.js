8668e9492f95c8cc9af2a21e1de51b6cf433e80f
Elias Junior <emcjunior@gmail.com>
2020-05-19 23:33:48 +0100

    Update CommonStyles2.js

--- src/styles/CommonStyles2.js
+++ src/styles/CommonStyles2.js
@@ -1,3 +1,8 @@
-import { green, red } from "@material-ui/core/colors";
+import { green, red, grey } from "@material-ui/core/colors";
 export const redColor = { color: red[500] };
 export const greenColor = { color: green[400] };
+export const tickedTitle = {
+  color: grey[400],
+  margin: "10px",
+  fontWeight: 700,
+};
