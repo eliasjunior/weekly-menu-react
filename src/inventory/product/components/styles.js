878ef304d7d75c426de1b76ee896f435ec1814f5
Elias Junior <emcjunior@gmail.com>
2020-05-19 23:27:45 +0100

    Update styles.js

--- src/inventory/product/components/styles.js
+++ src/inventory/product/components/styles.js
@@ -1,12 +1,21 @@
-import { green, red } from "@material-ui/core/colors";
+import { green, red, grey } from "@material-ui/core/colors";
+//TODO make common color global, move to /style
 export const styles = {
   saveIcon: {
-    color: green[400]
+    color: green[400],
   },
   editIcon: {
-    color: green[400]
+    color: green[400],
   },
   deleteIcon: {
-    color: red[500]
-  }
+    color: red[500],
+  },
+  greyLight: {
+    color: grey[200],
+  },
+  textItemListTicked: {
+    textDecoration: "line-through",
+    color: grey[500],
+    backgroundColor: grey[200],
+  },
 };
