47e11c6a9a55cd66fa758ba5edd59827f6bacb1d
Elias Junior <emcjunior@gmail.com>
2020-05-25 23:22:26 +0100

    Update ShoppingHistoryReducer.js

--- src/app-redux/reducers/ShoppingHistoryReducer.js
+++ src/app-redux/reducers/ShoppingHistoryReducer.js
@@ -1,5 +1,6 @@
 import {
   CREATE_SHOPPING_LIST,
+  UPDATE_SHOPPING_LIST,
   FETCH_SHOPPING_HISTORY,
 } from "app-redux/actions/ShoppingHistoryAction";
 
@@ -8,6 +9,8 @@ const initialState = [];
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case CREATE_SHOPPING_LIST:
+      return [...state, payload];
+    case UPDATE_SHOPPING_LIST:
       return [...state, payload];
     case FETCH_SHOPPING_HISTORY:
       return [...payload.shoppingHistory];
