f0889b4abbd0ab2fc37713b68da8f2edac38e99b
Elias Junior <emcjunior@gmail.com>
2020-05-25 23:19:12 +0100

    Update ProductFormAction.js

--- src/app-redux/actions/ProductFormAction.js
+++ src/app-redux/actions/ProductFormAction.js
@@ -1,43 +1,31 @@
-export const PRODUCT_EDIT = "PRODUCT_EDIT";
-export const PRODUCT_VIEW = "PRODUCT_VIEW";
-export const PRODUCT_SELECTION = "PRODUCT_SELECTION";
+export const PRODUCT_EDIT_VIEW = "PRODUCT_EDIT_VIEW";
+export const PRODUCT_SELECTION_VIEW = "PRODUCT_SELECTION_VIEW";
 export const PROD_PICKING_VIEW = "PROD_PICKING_VIEW";
 export const PROD_SHOPPING_LIST_VIEW = "PROD_SHOPPING_LIST_VIEW";
 
-export const FORM_VIEW_EDIT = "FORM_VIEW_EDIT";
-export const FORM_VIEW_LABEL = "FORM_VIEW_LABEL";
+export const PROD_LABEL = "PROD_LABEL";
 
-export const BTN_EDIT_MODE = "BTN_EDIT_MODE";
-export const BTN_VIEW_MODE = "BTN_VIEW_MODE";
+export const BTN_CRUD = "PRODUCT_CRUD";
 export const BTN_SELECTION = "BTN_SELECTION";
 export const BTN_SHOPPING_SELECTION = "BTN_SHOPPING_SELECTION";
 export const BTN_PICK_PROD = "BTN_PICK_PROD";
 export const BTN_QDT_INFO = "BTN_QDT_INFO";
 
-export function formViewAction() {
-  return {
-    type: PRODUCT_VIEW,
-  };
-}
-
 export function formEditAction() {
   return {
-    type: PRODUCT_EDIT,
+    type: PRODUCT_EDIT_VIEW,
   };
 }
-
 export function formSelectionAction() {
   return {
-    type: PRODUCT_SELECTION,
+    type: PRODUCT_SELECTION_VIEW,
   };
 }
-//REVIEW if need it and formSelectionAction
 export function formPickAction() {
   return {
     type: PROD_PICKING_VIEW,
   };
 }
-
 export function formShoppingAction() {
   return {
     type: PROD_SHOPPING_LIST_VIEW,
