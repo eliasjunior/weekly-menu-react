11021dc885c989f6131b63800244631d30f99e15
Elias Junior <emcjunior@gmail.com>
2020-05-26 22:47:26 +0100

    Update ListFilterReducer.test.js

--- src/app-redux/reducers/ListFilterReducer.test.js
+++ src/app-redux/reducers/ListFilterReducer.test.js
@@ -3,7 +3,10 @@ import { SET_FILTER_NAME } from "../actions/ListFilterAction";
 
 describe("Visibility filter", () => {
   it("should return the initial state", () => {
-    expect(reducer(undefined, {})).toEqual({ displayList: [], textFilter: "" });
+    expect(reducer(undefined, {})).toEqual({
+      displayList: [],
+      payload: { textFilter: "", listDB: [] },
+    });
   });
   it("should filter base on the text filter passed", () => {
     const categories = [
@@ -17,19 +20,14 @@ describe("Visibility filter", () => {
       },
     ];
 
-    const resultState = reducer([], {
+    const resultState = reducer(undefined, {
       type: SET_FILTER_NAME,
-      categories,
-      textFilter: "Or",
+      payload: { textFilter: "Or", listDB: categories },
     });
-    const catsFilter = resultState;
-    expect(categories.length).toEqual(2);
-    expect(categories[0].products.length).toEqual(1);
-    expect(categories[1].products.length).toEqual(2);
 
-    expect(catsFilter.length).toEqual(1);
-    expect(catsFilter[0].name).toEqual("Fruits");
-    expect(catsFilter[0].products.length).toEqual(1);
-    expect(catsFilter[0].products[0].name).toEqual("Oranges");
+    expect(resultState).toBeDefined();
+    expect(resultState.textFilter).toEqual("Or");
+    expect(resultState.displayList.length).toEqual(1);
+    expect(resultState.displayList[0].products[0].name).toEqual("Oranges");
   });
 });
