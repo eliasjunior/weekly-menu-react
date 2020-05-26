cff9c1143d76aa4265dc94ec39b65cb1b2bb1128
Elias Junior <emcjunior@gmail.com>
2020-05-26 22:44:24 +0100

    Update ListFilterAction.test.js

--- src/app-redux/actions/ListFilterAction.test.js
+++ src/app-redux/actions/ListFilterAction.test.js
@@ -1,12 +1,12 @@
-import { SET_FILTER_NAME, inventoryFilter } from "./ListFilterAction";
+import { SET_FILTER_NAME, listFilterAction } from "./ListFilterAction";
 
 describe("actions visibility filter", () => {
   it("should filter the display category", () => {
     const expectedAction = {
       type: SET_FILTER_NAME,
-      textFilter: "ba"
+      payload: { textFilter: "ba" },
     };
 
-    expect(inventoryFilter("ba")).toEqual(expectedAction);
+    expect(listFilterAction("ba")).toEqual(expectedAction);
   });
 });
