a36ad16d8c894440d69af21f02bd6848aec4b886
Elias Junior <emcjunior@gmail.com>
2020-05-26 22:49:30 +0100

    Update ShoppingListData.js

--- src/app-redux/reducers/mock-data/ShoppingListData.js
+++ src/app-redux/reducers/mock-data/ShoppingListData.js
@@ -1,5 +1,28 @@
 export const test1 = {
+  state: {
+    categories: {
+      byId: {
+        c_02: {
+          id: "c_02",
+          prods: ["p_01"],
+        },
+      },
+    },
+    products: {
+      byId: {
+        p_01: {
+          id: "p_01",
+        },
+      },
+      selected: [],
+    },
+    recipes: {
+      byId: {},
+    },
+  },
   expected: {
+    id: undefined,
+    name: undefined,
     categories: {
       byId: {
         c_02: {
@@ -43,6 +66,8 @@ export const test2 = {
     },
   },
   expected: {
+    id: undefined,
+    name: undefined,
     categories: {
       byId: {
         c_02: {
@@ -58,6 +83,7 @@ export const test2 = {
         },
         p_02: {
           id: "p_02",
+          recipes: [],
         },
       },
       selected: ["p_01", "p_02"],
@@ -91,6 +117,8 @@ export const test3 = {
     },
   },
   expected: {
+    id: undefined,
+    name: undefined,
     categories: {
       byId: {
         c_02: {
@@ -111,7 +139,35 @@ export const test3 = {
 };
 
 export const test4 = {
+  state: {
+    categories: {
+      byId: {
+        c_02: {
+          id: "c_02",
+          prods: ["p_01"],
+        },
+        c_04: {
+          id: "c_04",
+          prods: ["p_03"],
+        },
+      },
+    },
+    products: {
+      byId: {
+        p_01: {
+          id: "p_01",
+        },
+        p_03: {
+          id: "p_03",
+        },
+      },
+      selected: [],
+    },
+    recipes: { byId: {} },
+  },
   expected: {
+    id: undefined,
+    name: undefined,
     categories: {
       byId: {
         c_02: {
@@ -153,6 +209,10 @@ export const test5 = {
           id: "c_02",
           prods: ["p_01", "p_02"],
         },
+        c_04: {
+          id: "c_04",
+          prods: ["p_03"],
+        },
       },
     },
     products: {
@@ -163,6 +223,9 @@ export const test5 = {
         p_02: {
           id: "p_02",
         },
+        p_03: {
+          id: "p_03",
+        },
       },
       selected: ["p_01", "p_02"],
     },
@@ -171,6 +234,8 @@ export const test5 = {
     },
   },
   expected: {
+    id: undefined,
+    name: undefined,
     categories: {
       byId: {
         c_02: {
@@ -244,6 +309,8 @@ export const test6 = {
     },
   },
   expected: {
+    id: undefined,
+    name: undefined,
     categories: {
       byId: {
         c_02: {
@@ -296,6 +363,8 @@ export const prodInSelectedAndRecipe = {
     },
   },
   expected: {
+    id: undefined,
+    name: undefined,
     categories: {
       byId: {
         drink: {
