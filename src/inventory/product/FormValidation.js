784b0b48522764c4b67e894a4df51e5bb22d5406
Elias Junior <emcjunior@gmail.com>
2020-10-27 15:26:27 +0000

    Update FormValidation.js

--- src/inventory/product/FormValidation.js
+++ src/inventory/product/FormValidation.js
@@ -1,7 +1,11 @@
 import { infoMessage } from "app-redux/actions/AlertHandlerAction";
-export function isProdFormValid({ name, dispatch }) {
+export function isProdFormValid({ name, dispatch, quantityType }) {
   if (!name) {
-    dispatch(infoMessage("You cannot save a product without a name!"));
+    dispatch(infoMessage("Product name is required."));
+    return false;
+  }
+  if (!quantityType) {
+    dispatch(infoMessage("Quantity type is required."));
     return false;
   }
   return true;
