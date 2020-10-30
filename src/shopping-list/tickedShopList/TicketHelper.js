4f1fa950b9e34a8a5f3338a8c0868f4e959e8dc0
Elias Junior <emcjunior@gmail.com>
2020-10-30 12:50:30 +0000

    Create TicketHelper.js

---
+++ src/shopping-list/tickedShopList/TicketHelper.js
@@ -0,0 +1,22 @@
+import { requiredParameter } from "common/Util";
+
+export default function TicketHelper({ getDeepCopy }) {
+  return {
+    filterByPicked: function ({
+      displayList = requiredParameter("displayList"),
+    }) {
+      const copiedList = getDeepCopy(displayList);
+      if (!copiedList) {
+        return [];
+      }
+      return copiedList.filter((cat) => {
+        if (!cat.products) {
+          requiredParameter("products from category");
+        }
+        const pickedProds = cat.products.filter((prod) => prod.picked);
+        cat.products = pickedProds;
+        return cat.products.length > 0;
+      });
+    },
+  };
+}
