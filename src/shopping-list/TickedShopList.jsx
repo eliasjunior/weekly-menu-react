a5e1af95e3463cd328535fddf5a8973623820004
Elias Junior <emcjunior@gmail.com>
2020-10-30 12:52:03 +0000

    Update and rename TickedShopList.js to TickedShopList.jsx

--- src/shopping-list/TickedShopList.jsx
+++ src/shopping-list/TickedShopList.jsx
@@ -1,22 +1,18 @@
 import React from "react";
-import { List, ListItem } from "@material-ui/core";
 import { useSelector } from "react-redux";
-import cloneDeep from "lodash.clonedeep";
+import { ListItem } from "@material-ui/core";
+import { List, Divider } from "@material-ui/core";
 import { tickedTitle } from "styles/CommonStyles2";
+import { buildPickedList } from "./ticketShopList";
 
 export default function TickedShopList() {
   const { displayList } = useSelector((state) => state.listFilter);
-
-  const pickedList = cloneDeep(displayList).filter((cat) => {
-    const pickedProds = cat.products.filter((prod) => prod.picked);
-    cat.products = pickedProds;
-    return cat.products.length > 0;
-  });
-  if (pickedList.length === 0) {
-    return "";
-  }
-  return (
+  const pickedList = buildPickedList(displayList);
+  return pickedList.length === 0 ? (
+    ""
+  ) : (
     <>
+      <Divider></Divider>
       <div style={tickedTitle}>Ticked</div>
       <List></List>
       {pickedList.map((cat) => {
