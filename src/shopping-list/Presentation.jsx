c6c4d14e369e528b130d27378d3d99189496a354
Elias Junior <emcjunior@gmail.com>
2020-10-31 00:17:08 +0000

    Create Presentation.jsx

---
+++ src/shopping-list/Presentation.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardMedia,
+  CardActionArea,
+  CardActions,
+} from "@material-ui/core";
+import ShoppingCreateBtns from "./ShoppingCreateBtns";
+const IMG_SRC = "/recipe/checklist.jpg";
+export default function Presentation() {
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          style={{ height: 440, margin: 10, borderRadius: 5 }}
+          image={IMG_SRC}
+          title="Paella dish"
+        ></CardMedia>
+      </CardActionArea>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          New Shopping List
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          You can add an entire recipe products to the shopping list or simply
+          add multiple products
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <ShoppingCreateBtns></ShoppingCreateBtns>
+      </CardActions>
+    </Card>
+  );
+}
